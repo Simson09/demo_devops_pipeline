50195bce30dde54aeafadae6ece6e941e0c87dfb
Simson09 <antosimson02@gmail.com>
2025-11-13 07:15:12 +0530

    fresh start

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "demo-devops-pipeline-7six3nine3676-simson"
+  acl    = "private"
+}
